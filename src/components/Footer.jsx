0b0531278c34af289d8b16b99a57a26d47f40df9
Sam <sambolton.kw@gmail.com>
2024-07-12 22:55:24 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer className="text-center py-4">
-      <p>&copy; 2024 Your Name. All Rights Reserved.</p>
+        <div className='container'>
+            <p>Connect with me!</p>
+            <a href="https://github.com/s-bolt" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://www.linkedin.com/in/sam-bolton-07b4a731/" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <FaLinkedin size={30} />
+        </a>
+        </div>
+      <p>&copy; 2024 Sam Bolton. All Rights Reserved.</p>
     </footer>
   );
 }

ea62b58514b1309eee4e149b6a5c51bda95c9949
Sam <sambolton.kw@gmail.com>
2024-11-05 17:44:40 -0500

    edit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
             <h2 className="p-3" > About Me</h2>
             <div className='about-me-text'>
             <p className="my-3 text-left p-3 mb-5">
-                 Hi! I’m Sam, a full-stack web developer with a passion for learning, tinkering, solving problems, and using code to create whatever my mind can conjure. After deciding to make a career change and completing a coding boot camp, I realized that web development offers the perfect blend of creativity and technical challenges, allowing me to express myself while building functional applications.
+                 Hi! I’m Sam, a full-stack developer with a passion for learning, tinkering, solving problems, and using code to create whatever my mind can conjure. After deciding to make a career change and completing a coding boot camp, I realized that being a developer offers the perfect blend of creativity and technical challenges, allowing me to express myself while building functional applications.
                 <br></br>
                 <br></br>
                  As a people person, I thrive on collaboration and enjoy working with teams to bring ideas to life. When I’m not coding, you can find me somewhere outdoors, on a hike, playing sand volleyball, or maybe at a baseball game. I’m always excited to connect with fellow developers and collaborate on innovative projects!
